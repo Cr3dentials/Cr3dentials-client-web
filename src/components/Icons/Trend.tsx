c55e1a64fe69ce8d12df1aba882159f8efd18779
PeterKwesiAnsah <ansahpeter123@yahoo.com>
2024-06-18 09:39:59 +0000

    chore: create new icons

---
+++ src/components/Icons/Trend.tsx
@@ -0,0 +1,22 @@
+const Trend = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+    >
+      <rect width="40" height="40" rx="20" fill="#E9F9F1" />
+      <path
+        d="M15 25L25 15M25 15H15M25 15V25"
+        stroke="#20C375"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default Trend
